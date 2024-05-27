286f76d08f71960833c3db1def4a1e7fabfae9d0
kaps <kasperjonassen@yahoo.dk>
2024-05-27 17:45:42 +0200

    hygge

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import session from 'express-session';
-import {logEveryoneOut, readLoggedIn, initPlayersJSON} from './playerDB.js'
-import {apiRouter, getGameStarted} from './routes/api.js'
-import {loginRouter, getQueue} from './routes/login.js'
+import express from "express";
+import session from "express-session";
+import { logEveryoneOut, readLoggedIn, initPlayersJSON } from "./playerDB.js";
+import { apiRouter, getGameStarted } from "./routes/api.js";
+import { loginRouter, getQueue } from "./routes/login.js";
 
 const app = express();
 app.set("view engine", "pug");
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.static('./assets')) 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./assets"));
 app.use(express.json());
+app.use(
+  session({
+    secret: "2EFA4E9B-26FE-4D2A-94FA-D51827DA2F8B",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 
-app.use(session({
-  secret: '2EFA4E9B-26FE-4D2A-94FA-D51827DA2F8B',
-  saveUninitialized: true,
-  resave: true
-}));
-
-//Middleware til login
-function checkLogin(req,res,next) {
+function checkLogin(req, res, next) {
   if (req.session.loggedIn) {
     next();
   } else {
-    if (req.path !== '/login' && req.path !== '/auth') {
-      res.redirect('/login');
+    if (req.path !== "/login" && req.path !== "/auth") {
+      res.redirect("/login");
     } else {
       next();
     }
@@ -32,82 +32,88 @@ function checkLogin(req,res,next) {
 
 async function controlAccess(req, res, next) {
   const users1 = await readLoggedIn();
-
-  let queue = getQueue()
-  let gameStarted = getGameStarted()
-
+  let queue = getQueue();
+  let gameStarted = getGameStarted();
   const playersInGame = users1.filter((e) => !queue.includes(e.name));
   const playersInQueue = queue;
-
-  const userInGame = playersInGame.some(player => player.name === req.session.name);
+  
+  const userInGame = playersInGame.some(
+    (player) => player.name === req.session.name
+  );
   const userInQueue = playersInQueue.includes(req.session.name);
 
-  if (userInGame && gameStarted && req.path !== '/' && !req.path.startsWith("/api")) {
-    return res.redirect('/');
-  } else if (userInQueue && gameStarted && req.path !== '/waitinglobby' && !req.path.startsWith("/api")) {
-    return res.redirect('/waitinglobby');
-  } else if (req.session.loggedIn && !gameStarted && req.path !== `/lobby/${req.session.name}` && !req.path.startsWith("/api")) {
+  if (
+    userInGame &&
+    gameStarted &&
+    req.path !== "/" &&
+    !req.path.startsWith("/api")
+  ) {
+    return res.redirect("/");
+  } else if (
+    userInQueue &&
+    gameStarted &&
+    req.path !== "/waitinglobby" &&
+    !req.path.startsWith("/api")
+  ) {
+    return res.redirect("/waitinglobby");
+  } else if (
+    req.session.loggedIn &&
+    !gameStarted &&
+    req.path !== `/lobby/${req.session.name}` &&
+    !req.path.startsWith("/api")
+  ) {
     return res.redirect(`/lobby/${req.session.name}`);
   } else {
     next();
   }
 }
 
-app.use(checkLogin)
-app.use(controlAccess)
-
+app.use(checkLogin);
+app.use(controlAccess);
 app.use(apiRouter);
 app.use(loginRouter);
 
-
-app.get("/",  (req, res) => {
-  res.render('yatzy');
+app.get("/", (req, res) => {
+  res.render("yatzy");
 });
-
-
 
 app.get("/waitinglobby", async (req, res) => {
   const name = req.session.name;
   const users1 = await readLoggedIn();
-
   let queue = getQueue();
-
   let playersInGame = users1;
-  playersInGame = playersInGame.filter((e) => !queue.includes(e.name))
-  
-  res.render('waitinglobby', { name: name, playersInGame: playersInGame, playersInQueue: queue });
-})
+  playersInGame = playersInGame.filter((e) => !queue.includes(e.name));
 
-app.get("/lobby/:name",  async (req, res) => {
+  res.render("waitinglobby", {
+    name: name,
+    playersInGame: playersInGame,
+    playersInQueue: queue,
+  });
+});
+
+app.get("/lobby/:name", async (req, res) => {
   const name = req.session.name;
   const users = await readLoggedIn();
-  res.render('lobby', { name: name, users: users });
+  res.render("lobby", { name: name, users: users });
 });
 
-
-
-
-app.get('/gameover', async (req, res) => {
-  let players = await fetch("http://localhost:6969/api/getPlayers"); //Man kan ikke bruge fetch på server side?
+app.get("/gameover", async (req, res) => {
+  let players = await fetch("http://localhost:6969/api/getPlayers");
   players = await players.json();
 
-  const winner = "winner"; // Function to determine the winner
-  const playerScores = players.scoreVals; 
-  
-  res.render('yatzy', { winner, playerScores });
+  const winner = "winner";
+  const playerScores = players.scoreVals;
+
+  res.render("yatzy", { winner, playerScores });
 });
 
-
-async function tempFunc(){ //Giver 'Unexpected end of JSON input' en gang imellem
-  await initPlayersJSON(); 
-  setTimeout(async ()=>{
+async function tempFunc() {
+  await initPlayersJSON();
+  setTimeout(async () => {
     await logEveryoneOut();
   }, 1000);
 }
 
-app.listen(
-  6969, tempFunc(),
-  () => {
-    console.log("Lytter på port 6969.. ");
-  }
-);
+app.listen(6969, tempFunc(), () => {
+  console.log("Lytter på port 6969.. ");
+});
--- playerDB.js
+++ playerDB.js
@@ -1,139 +1,157 @@
-import fs, { read } from 'fs';
+import fs, { read } from "fs";
 
 async function readFile() {
-    let fileContent = await fs.promises.readFile("players.json", {encoding: 'utf-8'});
-    //console.log(fileContent);
-    return JSON.parse(fileContent);
+  let fileContent = await fs.promises.readFile("players.json", {
+    encoding: "utf-8",
+  });
+  return JSON.parse(fileContent);
 }
 
-function createPlayerObject(name){
-    return {name: name, loggedIn: false,
-        scoreVals: {
-            dicevals: [6, 6, 6, 6, 6],
-            ones: -1,
-            twos:  -1,
-            threes:  -1,
-            fours:  -1,
-            fives:  -1,
-            sixes:  -1,
-            onePair:  -1,
-            twoPairs:  -1,
-            threeSame:  -1,
-            fourSame:  -1,
-            fullHouse:  -1,
-            smallStraight:  -1,
-            largeStraight:  -1,
-            chance:  -1,
-            yatzy:  -1,
-            total: 0
-            }
-    };
+function createPlayerObject(name) {
+  return {
+    name: name,
+    loggedIn: false,
+    scoreVals: {
+      dicevals: [6, 6, 6, 6, 6],
+      ones: -1,
+      twos: -1,
+      threes: -1,
+      fours: -1,
+      fives: -1,
+      sixes: -1,
+      onePair: -1,
+      twoPairs: -1,
+      threeSame: -1,
+      fourSame: -1,
+      fullHouse: -1,
+      smallStraight: -1,
+      largeStraight: -1,
+      chance: -1,
+      yatzy: -1,
+      total: 0,
+    },
+  };
 }
 
-async function updatePlayerScore(name, field, point, total){
-    let currentDB = await readFile();
+async function updatePlayerScore(name, field, point, total) {
+  let currentDB = await readFile();
+  let playerIndex = -1;
 
-    let playerIndex = -1;
+  for (let i = 0; i < currentDB.length; i++) {
+    if (currentDB[i].name == name) playerIndex = i;
+  }
 
-    for(let i = 0 ; i<currentDB.length ; i++){
-        if(currentDB[i].name==name) playerIndex = i;
-    }
+  if (playerIndex == -1) {
+    console.log("EROOR player not found");
+    return undefined;
+  }
 
-    if(playerIndex==-1){
-        console.log("EROOR player not found");
-        return undefined;
-    }
+  console.log("Test204: " + field + " " + point);
 
-    console.log("Test204: " + field + " " + point);
+  Object.defineProperty(currentDB[playerIndex].scoreVals, field, {
+    value: point,
+    writable: true,
+  });
 
-    Object.defineProperty(currentDB[playerIndex].scoreVals, field, {value: point, writable: true});
-    Object.defineProperty(currentDB[playerIndex].scoreVals, "total", {value: total, writable: true});
+  Object.defineProperty(currentDB[playerIndex].scoreVals, "total", {
+    value: total,
+    writable: true,
+  });
 
-    let existingPlayer = JSON.stringify(currentDB);
-    await fs.promises.writeFile("players.json", existingPlayer, {encoding: "utf-8"});
+  let existingPlayer = JSON.stringify(currentDB);
+  await fs.promises.writeFile("players.json", existingPlayer, {
+    encoding: "utf-8",
+  });
 }
 
 async function addPlayer(playerObject) {
-    let existingPlayer = await readFile();
-    existingPlayer.push(playerObject);
-    existingPlayer = JSON.stringify(existingPlayer);
-    await fs.promises.writeFile("players.json", existingPlayer, {encoding: "utf-8"})
+  let existingPlayer = await readFile();
+  existingPlayer.push(playerObject);
+  existingPlayer = JSON.stringify(existingPlayer);
+
+  await fs.promises.writeFile("players.json", existingPlayer, {
+    encoding: "utf-8",
+  });
 }
 
 async function doesPlayerExist(name) {
-    let players = await readFile()
-    for(let player of players) {
-        if(player.name == name) {
-            return true;
-        }
+  let players = await readFile();
+  for (let player of players) {
+    if (player.name == name) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 async function loginAllowed(name) {
-    let players = await readFile();
-    for(let player of players) {
-        if(player.name == name && !player.loggedIn) {
-            return true;
-        }
+  let players = await readFile();
+  for (let player of players) {
+    if (player.name == name && !player.loggedIn) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
-//Assumes login allowed
-async function logPlayerIn(name){
-    let players = await readFile();
-    for (let player of players) {
-      if (player.name == name) player.loggedIn=true;
-    }
-    let existingPlayer = JSON.stringify(players);
-    await fs.promises.writeFile("players.json", existingPlayer, {
-      encoding: "utf-8",
-    });
+async function logPlayerIn(name) {
+  let players = await readFile();
+  for (let player of players) {
+    if (player.name == name) player.loggedIn = true;
+  }
+  let existingPlayer = JSON.stringify(players);
+  await fs.promises.writeFile("players.json", existingPlayer, {
+    encoding: "utf-8",
+  });
 }
 
-async function logEveryoneOut(){
-    let players = await readFile();
+async function logEveryoneOut() {
+  let players = await readFile();
+  for (let player of players) {
+    player.loggedIn = false;
+  }
 
-    for(let player of players){
-        player.loggedIn = false;
-    }
-
-   let existingPlayer = JSON.stringify(players);
-    await fs.promises.writeFile("players.json", existingPlayer, {
-      encoding: "utf-8",
-    });
+  let existingPlayer = JSON.stringify(players);
+  await fs.promises.writeFile("players.json", existingPlayer, {
+    encoding: "utf-8",
+  });
 }
 
 async function readLoggedIn() {
-    const jsonData = await readFile();
-    let loggedInUsers = jsonData.filter(user => user.loggedIn)
-    return loggedInUsers;
+  const jsonData = await readFile();
+  let loggedInUsers = jsonData.filter((user) => user.loggedIn);
+  return loggedInUsers;
 }
 
-
-async function initPlayersJSON(){
-   await fs.promises.readFile("players.json", {encoding: 'utf-8'})
-    .then((...args)=>{
-        console.log("players.json already initialized.");
-    })
-    .catch((...args)=>{
-        let defaultPlayers = [createPlayerObject("Player One"), createPlayerObject("Player Two")]
-
-        fs.writeFile("players.json", JSON.stringify(defaultPlayers), (err)=>{
-            if(err) console.log(err);
-            else console.log("players.json initialized");
-        })
-    })
+async function initPlayersJSON() {
+  try {
+    await fs.promises.readFile("players.json", { encoding: "utf-8" });
+    console.log("players.json already initialized.");
+  } catch (err) {
+    fs.writeFile("players.json", JSON.stringify([]), (err) => {
+      if (err) console.log(err);
+      else console.log("players.json initialized");
+    });
+  }
 }
-
 
 async function playerCount() {
-    const users = await readLoggedIn();
-    let playerCount = users.length;
+  const users = await readLoggedIn();
+  let playerCount = users.length;
 
-    return playerCount;
+  return playerCount;
 }
 
-
-export {readFile, addPlayer, loginAllowed, doesPlayerExist,logPlayerIn, logEveryoneOut, readLoggedIn, initPlayersJSON, createPlayerObject, playerCount, updatePlayerScore}
+export {
+  readFile,
+  addPlayer,
+  loginAllowed,
+  doesPlayerExist,
+  logPlayerIn,
+  logEveryoneOut,
+  readLoggedIn,
+  initPlayersJSON,
+  createPlayerObject,
+  playerCount,
+  updatePlayerScore,
+};
